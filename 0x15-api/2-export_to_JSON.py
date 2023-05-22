de07b8f0c1beb2c3c67bf0c2d125be4d4cd49f23
Olumide Michael Ojo <olumide.m.ojo@gmail.com>
2023-05-22 12:59:10 -0700

    updated permission

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to JSON format."""
+"""Exports to-do list data for an employee to JSON format"""
+
 import json
 import requests
 import sys
 
+
 if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
-
-    with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump({user_id: [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": username
-            } for t in todos]}, jsonfile)
+    user = requests.get("{}users/{}".format(url, user_id)).json()
+    tasks = requests.get("{}users/{}/todos".format(url, user_id)).json()
+    data_dict = {
+                user_id: [{"task": task.get("title"),
+                           "completed": task.get("completed"),
+                           "username": user.get("username")} for task in tasks]
+            }
+    # open a new JSON file in write mode
+    with open("{}.json".format(user_id), "w", newline="") as file:
+        json.dump(data_dict, file)
